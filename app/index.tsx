2fd792384d3c36aa1176a12d93d44c95855e7847
Italo Stuardo <ingeniero.stuardo@gmail.com>
2025-04-10 19:49:32 -0600

    Adding required images, working on the top part of the app

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,64 @@
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+  Image,
+} from "react-native";
+
+import Menu from "../assets/images/Menu.svg";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <ImageBackground
+      source={require("../assets/images/Background.png")}
+      style={styles.background}
+      resizeMode="cover"
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <SafeAreaView style={{ flex: 1 }}>
+        <TouchableOpacity style={styles.menu}>
+          <Menu />
+        </TouchableOpacity>
+        <View style={styles.imageContainer}>
+          <Image source={require("../assets/images/Avatar.png")} />
+          <View style={styles.textContainer}>
+            <Text style={styles.name}>Georgia</Text>
+            <Text style={styles.text}>Let's start designing</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-end",
+  },
+  text: {
+    color: "#31312B",
+    fontSize: 24,
+    fontWeight: "300",
+  },
+  menu: {
+    alignSelf: "flex-end",
+    marginRight: 30,
+    marginTop: 20,
+  },
+  imageContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  name: {
+    color: "#848280",
+    fontSize: 24,
+  },
+  textContainer: {
+    marginLeft: -20,
+  },
+});
